0dd927ae016ecba51c06d97e7eaadec9af42b974
kr-younghoon <sullanta0802@gmail.com>
2023-12-04 03:28:32 +0900

    [#3] Edit: 기능제작을 위한 버튼 구현

--- src/Components/Button/RadioBoxBtn.js
+++ src/Components/Button/RadioBoxBtn.js
@@ -1,8 +1,10 @@
-function RadioBoxBtn(props) {
+import styles from './RadioBoxBtn.module.css';
+
+function RadioBoxBtn({ emoge, ide, onChange }) {
     return (
-        <div>
-            <input type="radio" />
-            <label htmlFor={props.emoge}>{props.emoge}</label>
+        <div className={styles.checkbox_radio}>
+            <input type="radio" id={ide} onChange={onChange} />
+            <label htmlFor={ide}>{emoge}</label>
         </div>
     );
 }
