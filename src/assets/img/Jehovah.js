0b1c9610f3aae0a24738866728f417c1d374fe8e
kr-younghoon <sullanta0802@gmail.com>
2023-12-20 16:54:20 +0900

    [#16] add: 이미지 추가

---
+++ src/assets/img/Jehovah.js
@@ -0,0 +1,20 @@
+import styles from './Jehovah.module.css';
+let JehovahPNG = require('./jehovah.png');
+let RectanglePNG = require('./Rectangle.png');
+
+function Jehovah() {
+    return (
+        <>
+            <div className={styles.jbody}>
+                <img src={JehovahPNG} alt="예수님몸통" />
+            </div>
+            <div className={styles.jarms}>
+                <img src={RectanglePNG} alt="예수님왼쪽팔" />
+
+                <img src={RectanglePNG} alt="예수님오른쪽팔" />
+            </div>
+        </>
+    );
+}
+
+export default Jehovah;
