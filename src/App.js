cfae748864c04733ab5248dea0810780855c8f3b
kr-younghoon <sullanta0802@gmail.com>
2023-12-22 01:50:06 +0900

    [#22] feat: 404 페이지

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LetterPage from './Pages/LetterPage';
 import IconPage from './Pages/IconPage';
+import NotFound from './Components/Error/NotFound';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/2" element={<IconPage />} />
                     <Route path="/3" element={<LetterPage />} />
+                    <Route path="/*" element={<NotFound />} />
                 </Routes>
             </Router>
         </>
---
+++ src/Components/Error/NotFound.js
@@ -0,0 +1,16 @@
+import PNG404 from '../../assets/img/404.png';
+import styles from './NotFound.module.css';
+
+function NotFound() {
+    return (
+        <>
+            <img src={PNG404} alt="404" />
+            <h1>문제가 발생했어요🥲</h1>
+            <h1>
+                <a href="/">메인 페이지로 이동하기</a>
+            </h1>
+        </>
+    );
+}
+
+export default NotFound;
