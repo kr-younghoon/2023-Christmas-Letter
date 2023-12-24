a9bd1dbfb113e8ff6e503ebf871db4f3dff62edd
kr-younghoon <sullanta0802@gmail.com>
2023-12-24 09:59:10 +0900

    [#27] feat: Data 처리

--- src/Pages/IconPage.js
+++ src/Pages/IconPage.js
@@ -1,14 +1,21 @@
+// src/Pages/IconPage.js
+
 import { useState } from 'react';
 import styles from './css/IconPage.module.css';
 import RadioBoxBtn from '../Components/Button/RadioBoxBtn';
 import BtnBackspace from '../assets/svg/BtnBackSpace';
 import Jehovah from '../assets/img/Jehovah';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-function StylingOptions() {
+function IconPage() {
     const emojiList = Array.from(
         { length: 348 },
         (_, index) => `emoji/emoji${index + 1}.png`
     );
+    const dispatch = useDispatch();
+
     const [selectEmoji, setSelectEmoji] = useState(emojiList[0]);
     const [disabled, setDisabled] = useState(false);
 
@@ -18,13 +25,21 @@ function StylingOptions() {
         console.log(`${value}`);
     };
 
+    const handleSave = () => {
+        // 이모지를 Redux 스토어에 저장
+        dispatch({ type: 'SET_PAGE2_DATA', payload: { selectEmoji } });
+    };
+    const page2Data = useSelector((state) => state.page2Data);
+
+    console.log('page2Data:', page2Data);
+
     return (
         <>
             <div className={styles.page}>
                 <div className={styles.header}>
-                    <a href="/">
+                    <Link to="/">
                         <BtnBackspace />
-                    </a>
+                    </Link>
                 </div>
                 <div className={styles.pageInline}>
                     <div className={styles.pageMidLine}>
@@ -61,12 +76,14 @@ function StylingOptions() {
                         </div>
                     </div>
                 </div>
-                <button className={styles.nextBtn}>
-                    <a href="/3">다음으로</a>
-                </button>
+                <Link to="/3">
+                    <button className={styles.nextBtn} onClick={handleSave}>
+                        다음으로
+                    </button>
+                </Link>
             </div>
         </>
     );
 }
 
-export default StylingOptions;
+export default IconPage;
--- src/Pages/LetterPage.js
+++ src/Pages/LetterPage.js
@@ -1,15 +1,43 @@
+// src/Pages/LetterPage.js
+
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Jehovah from '../assets/img/Jehovah';
 import BtnBackspace from '../assets/svg/BtnBackSpace';
 import styles from './css/LetterPage.module.css';
+import { Link } from 'react-router-dom';
 
 function LetterPage() {
+    const dispatch = useDispatch();
+
+    // React 상태를 사용하여 입력된 값을 관리합니다.
+    const [writerName, setWriterName] = useState('');
+    const [letterContent, setLetterContent] = useState('');
+
+    // 저장 버튼이 클릭될 때 실행되는 함수
+    const handleSave = () => {
+        // Redux 스토어에 페이지 3의 데이터를 저장합니다.
+        console.log(
+            'writerName : ',
+            writerName,
+            ' letterContent : ',
+            letterContent
+        );
+        dispatch({
+            type: 'SET_PAGE3_DATA',
+            payload: { writerName, letterContent },
+        });
+    };
+
+    const isValid = writerName.trim() !== '' && letterContent.trim() !== '';
+
     return (
         <>
             <div className={styles.page}>
                 <div className={styles.header}>
-                    <a href="/">
+                    <Link to="/2">
                         <BtnBackspace />
-                    </a>
+                    </Link>
                 </div>
                 <div className={styles.pageInline}>
                     <div className={styles.pageMidLine}>
@@ -31,6 +59,10 @@ function LetterPage() {
                                     <input
                                         type="text"
                                         placeholder="작성자 이름"
+                                        value={writerName}
+                                        onChange={(e) =>
+                                            setWriterName(e.target.value)
+                                        }
                                     ></input>
                                 </div>
                                 <div className={styles.page3_input_comp}>
@@ -38,15 +70,27 @@ function LetterPage() {
                                     <textarea
                                         type="text"
                                         placeholder="편지 내용"
+                                        value={letterContent}
+                                        onChange={(e) =>
+                                            setLetterContent(e.target.value)
+                                        }
                                     ></textarea>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <button className={styles.nextBtn}>
-                    <a href="/4">다음으로</a>
-                </button>
+                {isValid ? (
+                    <Link to="/4">
+                        <button className={styles.nextBtn} onClick={handleSave}>
+                            다음으로
+                        </button>
+                    </Link>
+                ) : (
+                    <div className={styles.inputWarning}>
+                        <div>다음으로</div>
+                    </div>
+                )}
             </div>
         </>
     );
